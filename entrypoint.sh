b966e6e1e578b65c25f54bad78600e5296896e67
Albrecht Weiche <a.weiche@opentalk.eu>
2024-08-27 16:57:05 +0200

    fix: make variables accessible in scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,13 @@ set -ex
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-GIT_CLIFF_CONFIG=${GIT_CLIFF_CONFIG:-$SCRIPT_DIR/cliff.toml}
+export GIT_CLIFF_CONFIG=${GIT_CLIFF_CONFIG:-$SCRIPT_DIR/cliff.toml}
 
 # Use GITLAB_API_URL if present or CI_API_V4_URL from the gitlab CI
-GITLAB_API_URL=${GITLAB_API_URL:-$CI_API_V4_URL}
+export GITLAB_API_URL=${GITLAB_API_URL:-$CI_API_V4_URL}
 
 # Use GITLAB_REPO if present or build CI_PROJECT_PATH from the gitlab CI
-GITLAB_REPO=${GITLAB_REPO:-$CI_PROJECT_PATH}
+export GITLAB_REPO=${GITLAB_REPO:-$CI_PROJECT_PATH}
 
 # we need to unset the GITLAB variales otherwise the context will be overwritten
 git-cliff -c $GIT_CLIFF_CONFIG --unreleased --context \
