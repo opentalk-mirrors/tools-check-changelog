f03f7c079543b7ce1355d5d941ee736bfa01f6aa
Albrecht Weiche <a.weiche@opentalk.eu>
2024-10-09 13:17:59 +0200

    ci: post changelog update thread

--- comment-changelog-additions.sh
+++ comment-changelog-additions.sh
@@ -1,44 +1,78 @@
 #!/bin/bash
 
-# This script adds a comment with the added changelog entries
+# This script takes a specific project and merge request as input, clones the
+# project, retrieves the changelog entries introduced by that merge request, and
+# posts them as a comment on the same merge request.
 
 set -e
 
-for cmd in git-cliff git-cliff-enhancer ot-gitlab-cli; do
+for cmd in git-cliff git-cliff-enhancer ot-gitlab-cli git awk; do
     if ! command -v $cmd &> /dev/null; then
         echo "Error: $cmd is not installed or not found in PATH" >&2
         exit 1
     fi
 done
 
+# Expected variables
+env_vars=("TARGET_REPO" "SOURCE_REPO" "GITLAB_MR" "SOURCE_BRANCH" "GITLAB_TOKEN")
+
+# Loop through the list and check each variable
+for var in "${env_vars[@]}"; do
+    if [ -z "${!var}" ]; then
+        echo "Error: Environment variable $var is not set." >&2
+        exit 1  # Exit with error if a variable is not set
+    fi
+done
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export GIT_CLIFF_CONFIG=${GIT_CLIFF_CONFIG:-$SCRIPT_DIR/cliff.toml}
-
-# Use GITLAB_API_URL if present or CI_API_V4_URL from the gitlab CI
+export GITLAB_MR_REF="$TARGET_REPO!$GITLAB_MR"
 export GITLAB_API_URL=${GITLAB_API_URL:-$CI_API_V4_URL}
-
-# Use GITLAB_REPO if present or build CI_PROJECT_PATH from the gitlab CI
-export GITLAB_REPO=${GITLAB_REPO:-$CI_PROJECT_PATH}
-export GITLAB_MR=${GITLAB_MR:-$CI_MERGE_REQUEST_IID}
-export GITLAB_MR_REF="$GITLAB_REPO!$GITLAB_MR"
+TARGET_BRANCH="main"
+echo " Creating changelog notification for
+Merge Request: $GITLAB_MR_REF
+Target Repository: $TARGET_REPO
+Target Branch: $TARGET_BRANCH
+Source Repository: $SOURCE_REPO
+Source Branch: $SOURCE_BRANCH
+"
 
 # Don't include any header in the output
 export GIT_CLIFF__CHANGELOG__HEADER=""
 
-temp_file=$(mktemp)
+REPO_REMOTE=$( ot-gitlab-cli project authorized-clone-url -p "$TARGET_REPO" )
+REPO_MR_REMOTE=$( ot-gitlab-cli project authorized-clone-url -p "$SOURCE_REPO" )
 
-# we need to unset the GITLAB variales otherwise the context will be overwritten
-MAIN=$( git rev-parse --abbrev-ref 'main@{u}' )
-git-cliff --config "$GIT_CLIFF_CONFIG" --context "$MAIN"..HEAD \
+# Temporary file for the changelog output
+temp_file=$( mktemp )
+
+# Temporary folder for the git repository
+temp_dir=$( mktemp -d )
+
+pushd "$temp_dir"
+
+# Setup the repository. Don't download any files, we only need the commit history.
+# Add the fork and fetch the branch that should get merged.
+git clone --no-checkout "$REPO_REMOTE" .
+git remote add mr-remote "$REPO_MR_REMOTE"
+git fetch mr-remote "$SOURCE_BRANCH"
+
+# We assume that the target branch is always main.
+TARGET_BRANCH=$( git rev-parse --abbrev-ref "$TARGET_BRANCH@{u}" )
+
+export GITLAB_REPO="$TARGET_REPO"
+git-cliff --config "$GIT_CLIFF_CONFIG" --context "$TARGET_BRANCH..mr-remote/$SOURCE_BRANCH" \
     | git-cliff-enhancer -vvvv \
     | git-cliff --config "$GIT_CLIFF_CONFIG" --from-context - -o "$temp_file"
 
+# We prepand every line with `> ` using awk
 echo -e "This MR will add the following changelog entries:
 
-\`\`\`md
-$(<"$temp_file")
-\`\`\`
-" | ot-gitlab-cli put-latest -vv -i -
+$(awk '{print "> "$0}' < "$temp_file")
+" | ot-gitlab-cli discussion put-latest -vv -i -
+
+popd
 
 rm -f "$temp_file"
+rm -rf "$temp_dir"
